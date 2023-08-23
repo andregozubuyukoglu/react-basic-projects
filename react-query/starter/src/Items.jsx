6e3d34b394cc14113dde985f7adfa3e5bd95fd3c
Andre <andregozubuyuk@gmail.com>
2023-08-24 00:57:35 +0300

    Updated files

---
+++ react-query/starter/src/Items.jsx
@@ -0,0 +1,26 @@
+import { useQuery } from "@tanstack/react-query"
+import SingleItem from "./SingleItem"
+import customFetch from "./utils"
+
+const Items = ({ items }) => {
+  const { isLoading, data } = useQuery({
+    queryKey: ["tasks"],
+    queryFn: async () => {
+      const { data } = await customFetch.get("/")
+      return data
+    },
+  })
+
+  if (isLoading) {
+    return <p style={{ marginTop: "1rem" }}>Loading...</p>
+  }
+
+  return (
+    <div className="items">
+      {data.data.taskList.map((item) => {
+        return <SingleItem key={item.id} item={item} />
+      })}
+    </div>
+  )
+}
+export default Items
