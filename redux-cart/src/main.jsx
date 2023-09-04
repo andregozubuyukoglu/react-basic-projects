54745a5cf026eee47b69ea86555e1750e5ed8314
Andre <andregozubuyuk@gmail.com>
2023-09-04 22:41:19 +0300

    Updated files

--- redux-cart/src/main.jsx
+++ redux-cart/src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
-import { Provider } from "react"
+
 import "./index.css"
 import App from "./App"
 import { store } from "./store"
+import { Provider } from "react-redux"
 
 const container = document.getElementById("root")
 const root = createRoot(container)
