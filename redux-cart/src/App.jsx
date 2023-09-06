dcdf64bba440e7b37a188a5e5f8788fdcc334ebb
Andre <andregozubuyuk@gmail.com>
2023-09-06 18:14:23 +0300

    Updated files

--- redux-cart/src/App.jsx
+++ redux-cart/src/App.jsx
@@ -1,9 +1,23 @@
+import { useDispatch, useSelector } from "react-redux"
+import CartContainer from "./components/CartContainer"
 import Navbar from "./components/Navbar"
+import { calculateTotals } from "./features/cart/cartSlice"
+import { useEffect } from "react"
+import Modal from "./components/Modal"
 
 function App() {
+  const { cartItems } = useSelector((store) => store.cart)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(calculateTotals())
+  }, [cartItems])
+
   return (
     <main>
+      <Modal />
       <Navbar />
+      <CartContainer />
     </main>
   )
 }
